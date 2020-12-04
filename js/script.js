1e94bdcd4d12a407a0a2a93eaf036b4ee0d5f390
priyanka <priya.nil1811@gmail.com>
2020-12-03 21:10:44 -0500

    random background color change exercise

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 // Here's a test channel number, check the console!
-console.log(getRandomColorChannel())
+//console.log(getRandomColorChannel())
 
 // 1) Built a colour string using `getRandomColorChannel()` for each of the three colour channels (R, G, B)
 
@@ -8,3 +8,30 @@ console.log(getRandomColorChannel())
 // 3) Use the new RGB value to update the label "rgb(###, ####, ###)" (replacing ###s with the Number values)
 
 // 4) Wrap the random colour procedure in a function named `setNewColour()`, call it from the `console` to test
+
+
+//let theMain = document.querySelector('.main')
+/*let redCol = 35
+let greenCol = 150
+let blueCol = 255*/
+
+ 
+//change style
+
+//theMain.style.setProperty(`background-color`, `rgb(255, 35, 150)`)
+    function setNewColour(){
+        let theMain = document.querySelector('.main')
+        let theButton = document.querySelector('.button')
+        let theText = document.querySelector('.text')
+
+        let redCol = getRandomColorChannel()
+        let greenCol = getRandomColorChannel()
+        let blueCol = getRandomColorChannel()
+        theMain.style.setProperty(`background-color`, `rgb(${redCol}, ${greenCol}, ${blueCol})`)
+        theButton.style.setProperty(`background-color`, `rgb(${redCol}, ${greenCol}, ${blueCol}, 0.5)`)
+        theText.style.setProperty(`background-color`, `rgb(${redCol}, ${greenCol}, ${blueCol}, 0.5)`)
+        theText.textContent = `rgb(${redCol}, ${greenCol}, ${blueCol})` 
+
+}
+
+
